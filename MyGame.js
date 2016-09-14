963047f508357d5df1f10c31b1204890cba081c5
vadimbulavski <zolsberh@mail.ru>
2016-09-14 17:27:48 +0300

    game2

--- MyGame.js
+++ MyGame.js
@@ -1,12 +1,15 @@
 /**
  * Created by admin on 13.09.2016.
  */
-var cols = 10;
-var rows = 10;
+var colsMin = 10;
+var rowsMin = 10;
+var colsMax = 100;
+var rowsMax = 100;
 var cross = "X";
 var toe = "O";
 var divField = document.getElementById('field');
 var field;
+var hod = 0;
 
 function creatField(col, row) {
     var str = "<table id='tableField' style='border-spacing: inherit'>";
@@ -24,22 +27,32 @@ function creatField(col, row) {
         for(var j = 0; j < col; ++j){
             var trElem = field.childNodes[i];
             var tdElem = trElem.childNodes[j];
-            var butEl = document.createElement('button');
+            var butEl = document.createElement('div');
             butEl.style.width = "50px";
             butEl.style.height = "50px";
+            butEl.style.position = "relative";
+            butEl.style.backgroundColor = "orange";
+            butEl.style.border = "1px solid";
+            butEl.style.textAlign = "center";
+            butEl.style.lineHeight = "50px";
+            butEl.style.fontFamily = "Times New Roman";
+            butEl.style.fontSize = "26pt";
+            butEl.style.fontWeight = "900";
+            butEl.onclick = next;
             tdElem.appendChild(butEl);
         }
     }
 
-    $('#field').fadeIn(3000);
+    $('#field').animate({height: "100%", opacity: 0.5}, "slow");
+    $('#field').animate({width: "100%", opacity: 1}, "slow");//fadeIn(3000);
 }
 
 function checkInput() {
     var colsUser = document.getElementById('col').value;
     var rowsUser = document.getElementById('row').value;
-    if(colsUser < cols || rowsUser < rows){
+    if(colsUser < colsMin || rowsUser < rowsMin || colsUser > colsMax || rowsUser > rowsMax){
         alert("Некорректный ввод! Будут применены стандартные настройки!");
-        creatField(cols, rows);
+        creatField(colsMin, rowsMin);
     }
     else
         creatField(colsUser, rowsUser);
@@ -49,7 +62,27 @@ var createButton = document.getElementById('start');
 createButton.onclick = checkInput;
 
 function next() {
-    var hod
+    if(this.textContent == "") {
+        var text = document.createElement('p');
+        text.style.margin = "0%";
+        text.style.display = "none";
+        this.appendChild(text);
+        $(this).animate({left: "50%", width: "2px"});
+        if (hod % 2 == 0) {
+            text.textContent = cross;
+            text.style.color = "red";
+            $(this).animate({width: "50px", left: "0%"});
+            $(text).fadeIn(1000);
+            hod++;
+        }
+        else {
+            text.textContent = toe;
+            text.style.color = "green";
+            $(this).animate({width: "50px", left: "0%"});
+            $(text).fadeIn(1000);
+            hod++;
+        }
+    }
 }
 
 
